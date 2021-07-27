faeed35119886dd7b6e42c4710bdec0cadc401db
NorbertRuff <ruffnorbert88@gmail.com>
2021-07-27 09:33:30 +0200

    [add] List all categories and supplier filtering

--- src/main/java/com/codecool/shop/service/ProductService.java
+++ src/main/java/com/codecool/shop/service/ProductService.java
@@ -2,18 +2,23 @@ package com.codecool.shop.service;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 
 import java.util.List;
 
 public class ProductService{
     private ProductDao productDao;
     private ProductCategoryDao productCategoryDao;
+    private SupplierDao productSupplierDao;
 
-    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao) {
+    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao, SupplierDao productSupplierDataStore) {
         this.productDao = productDao;
         this.productCategoryDao = productCategoryDao;
+        this.productSupplierDao = productSupplierDataStore;
+
     }
 
     public ProductCategory getProductCategory(int categoryId){
@@ -23,6 +28,15 @@ public class ProductService{
     public List<Product> getProductsForCategory(int categoryId){
         var category = productCategoryDao.find(categoryId);
         return productDao.getBy(category);
+    }
+
+    public List<Product> getProductsForSupplier(int supplierId){
+        var supplier = productSupplierDao.find(supplierId);
+        return productDao.getBy(supplier);
+    }
+
+    public List<ProductCategory> getAllCategory(){
+        return productCategoryDao.getAll();
     }
 
 
