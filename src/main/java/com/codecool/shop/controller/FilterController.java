d95b31e837afa0c0c1984c7955491122708b7e7b
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-07-28 11:44:43 +0200

    [delete] unused classes

--- src/main/java/com/codecool/shop/controller/FilterController.java
+++ src/main/java/com/codecool/shop/controller/FilterController.java
@@ -1,6 +1,5 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.controller.filter.FilterUtil;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.ShoppingCartDao;
@@ -11,6 +10,8 @@ import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.product.Product;
 import com.codecool.shop.service.ProductService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 
@@ -66,4 +68,34 @@ public class FilterController extends HttpServlet {
 
     }
 
+    public static class FilterUtil {
+
+        public static int getRequestedId(HttpServletRequest req){
+            // TODO make sure not int values are handled as well
+            String path = req.getPathInfo();
+            String[] parts = path.split("/");
+            return Integer.parseInt(parts[1]);
+        }
+
+        public static String jsonifyData(List<Product> productsByCategory) {
+            Gson gson = new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .setPrettyPrinting()
+                .create();
+
+            return gson.toJson(productsByCategory);
+        }
+
+        public static void setResponseHeader(HttpServletResponse resp){
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+        }
+
+        public static void sendJSONResponse(HttpServletResponse resp, String serializedProducts) throws IOException {
+            PrintWriter out = resp.getWriter();
+            out.write(serializedProducts);
+            out.flush();
+        }
+
+    }
 }
