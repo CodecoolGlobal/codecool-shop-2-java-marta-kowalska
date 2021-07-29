35b73357456a1477b60f34f3386c495905c3fe8a
NorbertRuff <ruffnorbert88@gmail.com>
2021-07-29 11:41:45 +0200

    [update] checkout page with form logic and backend implementation

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -1,13 +1,8 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.ShoppingCartDao;
-import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.dao.*;
+import com.codecool.shop.dao.implementation.*;
+import com.codecool.shop.model.Order;
 import com.codecool.shop.service.ProductService;
 import com.codecool.shop.config.TemplateEngineUtil;
 import org.thymeleaf.TemplateEngine;
@@ -24,12 +19,20 @@ import java.util.Map;
 
 @WebServlet(urlPatterns = {"/checkout"})
 public class CheckoutController extends HttpServlet {
+    ShoppingCartDao shoppingCart = ShoppingCartDaoMem.getInstance();
+    OrderDao orderDao = OrderDaoMem.getInstance();
+
+    ProductService productService = new ProductService(shoppingCart, orderDao);
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        context.setVariable("summary", productService.getShoppingCart().getShoppingCartSummary());
 
         // // Alternative setting of the template context
         // Map<String, Object> params = new HashMap<>();
@@ -42,9 +45,6 @@ public class CheckoutController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext context = new WebContext(req, resp, req.getServletContext());
-
         String firstName = req.getParameter("fname");
         String lastName = req.getParameter("lname");
         String email = req.getParameter("email");
@@ -53,12 +53,17 @@ public class CheckoutController extends HttpServlet {
         String city = req.getParameter("city");
         String zipcode = req.getParameter("zipcode");
         String address = req.getParameter("address");
+
         String billingCountry = req.getParameter("b-country");
         String billingCity = req.getParameter("b-city");
         String billingZipcode = req.getParameter("b-zipcode");
         String billingAddress = req.getParameter("b-address");
 
-        engine.process("product/checkout.html", context, resp.getWriter());
+        orderDao.add(new Order(firstName, lastName, email, phoneNumber, country, city, zipcode, address));
+
+        shoppingCart.deleteCart();
+
+        resp.sendRedirect("/");
     }
 
 }
