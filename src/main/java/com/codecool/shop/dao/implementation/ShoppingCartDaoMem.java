9c581f1de32cb6583ac5ad58a94b20f278fa9e6c
NorbertRuff <ruffnorbert88@gmail.com>
2021-07-29 11:40:59 +0200

    [update] add method for clearing cart after succesfull order

--- src/main/java/com/codecool/shop/dao/implementation/ShoppingCartDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/ShoppingCartDaoMem.java
@@ -44,4 +44,9 @@ public class ShoppingCartDaoMem implements ShoppingCartDao {
     public ShoppingCart getCart() {
        return shoppingCart.getShoppingCart();
     }
+
+    @Override
+    public void deleteCart() {
+        shoppingCart.clearCart();
+    }
 }
