af1361aa954b17b36fbab54953d475108c2fa196
Alex Kovács <kovacsalex95@gmail.com>
2021-07-29 15:38:55 +0200

    [update] cart, overall layout, and color changes vol.1

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,5 +1,81 @@
 var userLoggedIn = true;
 
+jQuery(document).ready(function() {
+    listenCheckboxStates();
+    listenMinicartIframeSizeChanges();
+});
+
+
+/////////////////////// DOM ///////////////////////
+/**
+ * @summary Adds a 'checked' class to the checkbox's parent element if the checkbox is checked.
+ */
+function listenCheckboxStates() {
+
+    let checkboxStateInputs = jQuery('.checkbox-state > input[type="checkbox"]');
+
+    checkboxStateInputs.each(function() {
+        let checkbox = jQuery(this);
+        checkbox.attr('checked', false);
+    });
+
+    checkboxStateInputs.change(function() {
+        let checkbox = jQuery(this);
+        let container = checkbox.parent();
+        let isChecked = checkbox.is(':checked');
+
+        checkbox.attr('checked', !isChecked ? false : 'checked');
+        container.setDomClass("checked", isChecked);
+    });
+
+}
+
+/**
+ * @summary Resizes an iframe automatically by its content.
+ * @param {Object} iframe The iframe itself
+ * @param {Boolean} forceMeasure We want to mease the content by force?
+ */
+function resizeIframe(iframe, forceMeasure = false) {
+
+    let contentHeight = iframe.attr('content-height');
+
+    if (forceMeasure || contentHeight === undefined || contentHeight === null || !contentHeight) {
+        contentHeight = Math.ceil(iframe[0].contentWindow.document.documentElement.scrollHeight);
+        iframe.attr('content-height', contentHeight);
+    }
+    else
+        contentHeight = parseInt(contentHeight);
+
+    let topOffset = iframe.offset().top;
+    let windowHeight = jQuery(window).height() - 10;
+
+    let targetHeight = Math.floor(Math.min(contentHeight, windowHeight - topOffset));
+
+    iframe[0].style.height = targetHeight + 'px';
+}
+
+/**
+ * @summary Updates the Mini cart iframe's height automatically
+ */
+function listenMinicartIframeSizeChanges() {
+    let iframes = jQuery('#mini-cart iframe');
+
+    iframes.on("load",function() {
+        let iframe = jQuery(this);
+        setTimeout(function() {
+            resizeIframe(iframe);
+            iframes.setDomClass('loaded', true);
+        }, 1000);
+    });
+
+    jQuery(window).resize(function() {
+        iframes.each(function() {
+            let iframe = jQuery(this);
+            resizeIframe(iframe);
+        });
+    });
+}
+
 /////////////////////// MISC ///////////////////////
 /**
  * @summary Gets a cookies by its name
@@ -31,11 +107,51 @@ function setCookie(name, value, hours = undefined) {
 
     document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
-function incrementIconCounter(){
-    let cartCounter = document.querySelector("#cart-counter");
-    cartCounter.innerText = parseInt(cartCounter.innerText)+1;
-    cartCounter.classList.add("animation");
-    setTimeout(() =>{
-        cartCounter.classList.remove("animation");
-        }, 800);
+
+/**
+ * @summary Sets a given class for an element by a Boolean
+ * @param {String} className The selected class as String
+ * @param {Boolean} has We want to use this class?
+ * @returns {jQuery} The selected object as jQuery
+ */
+jQuery.prototype.setDomClass = function(className, has = true) {
+    let obj = jQuery(this);
+
+    try {
+        if (obj !== undefined) {
+            if (obj.length === 1) {
+                if (obj.hasClass(className) && !has)
+                    obj.removeClass(className);
+
+                if (!obj.hasClass(className) && has)
+                    obj.addClass(className);
+            }
+            else {
+                obj.each(function () {
+                    let _obj = jQuery(this);
+
+                    if (_obj.hasClass(className) && !has)
+                        _obj.removeClass(className);
+
+                    if (!_obj.hasClass(className) && has)
+                        _obj.addClass(className);
+                });
+            }
+        }
+    } catch(e) {
+        // its okay
+    }
+
+    return obj;
+}
+
+/**
+ * @summary Sets a class if a boolean is true, and one if its false.
+ * @param {String} classTrue The class we want to use if the Boolean is TRUE
+ * @param {String} classFalse The class we want to use if the Boolean is FALSE
+ * @param {Boolean} has The boolean itself
+ * @returns {jQuery} The selected object as jQuery
+ */
+jQuery.prototype.flipDomClass = function(classTrue, classFalse, has = true) {
+    return this.setDomClass(classTrue, has).setDomClass(classFalse, !has);
 }
