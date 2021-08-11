eae3d65cbc1df436ca05ebba316329c394d05db3
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-11 19:12:40 +0200

    [add] test for get category method

--- src/test/java/ProductServiceTest.java
+++ src/test/java/ProductServiceTest.java
@@ -1,39 +1,59 @@
 import com.codecool.shop.dao.*;
+import com.codecool.shop.model.product.ProductCategory;
 import com.codecool.shop.service.ProductService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
+import org.mockito.internal.matchers.Null;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 
 class ProductServiceTest {
-    private ProductDao productDao;
-    private ProductCategoryDao productCategoryDao;
-    private SupplierDao productSupplierDao;
-    private ShoppingCartDao shoppingCart;
+    private ProductDao productDaoMock;
+    private ProductCategoryDao productCategoryDaoMock;
+    private SupplierDao productSupplierDaoMock;
+    private ShoppingCartDao shoppingCartMock;
+    ProductService productService;
+    private OrderDao orderDaoMock;
 
-    private OrderDao orderDao;
+    ProductCategory dummyCategory = new ProductCategory("dummy");
 
 
     @BeforeEach
     void setUp() {
-        productDao = mock(ProductDao.class);
-        productCategoryDao = mock(ProductCategoryDao.class);
-        productSupplierDao = mock(SupplierDao.class);
-        shoppingCart = mock(ShoppingCartDao.class);
-        orderDao = mock(OrderDao.class);
-        ProductService productService = new ProductService(productDao,productCategoryDao,productSupplierDao,shoppingCart);
+        productDaoMock = mock(ProductDao.class);
+        productCategoryDaoMock = mock(ProductCategoryDao.class);
+        productSupplierDaoMock = mock(SupplierDao.class);
+        shoppingCartMock = mock(ShoppingCartDao.class);
+        orderDaoMock = mock(OrderDao.class);
+        productService = new ProductService(productDaoMock,productCategoryDaoMock,productSupplierDaoMock,shoppingCartMock);
     }
 
     @AfterEach
     void tearDown() {
     }
 
+    //TODO Így kéne tesztelni vagy most hogy? Confuused...:D
+    @Test
+    void givenProductCategory_WhenProductCategoryIdExists_ThReturnCategory() {
+        when(productCategoryDaoMock
+                .find(Mockito.anyInt()))
+                .thenReturn(dummyCategory);
+       var result =  productService.getProductCategory(1);
+       assertEquals(dummyCategory, result);
+    }
 
     @Test
-    void getProductCategory_productCategoryIsNull_returnNull() {
-
+    void givenProductCategory_WhenProductCategoryIdNotExists_ThReturnNull() {
+        when(productCategoryDaoMock
+                .find(Mockito.anyInt()))
+                .thenReturn(null);
+       var result =  productService.getProductCategory(1);
+        assertNull(result);
     }
 
     @Test
