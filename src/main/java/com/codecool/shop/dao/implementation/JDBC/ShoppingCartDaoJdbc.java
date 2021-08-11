a9d94473c9ffb480197e084c7f1b21a33488bc58
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-08-11 21:23:51 +0200

    [add] required methods

--- src/main/java/com/codecool/shop/dao/implementation/JDBC/ShoppingCartDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/JDBC/ShoppingCartDaoJdbc.java
@@ -39,14 +39,20 @@ public class ShoppingCartDaoJdbc implements ShoppingCartDao {
     public void add(Product product) {
         try (Connection conn = dataSource.getConnection()) {
             if(getProductQty(product) == 0){
-                String sql = "INSERT INTO cart_item VALUES (DEFAULT, ?, ?, ?, ?, ?)";
+                String sql = "INSERT INTO cart_item VALUES (DEFAULT, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
                 PreparedStatement statement = conn.prepareStatement(sql);
                 statement.setFloat(1, product.getDefaultPrice());
                 statement.setString(2, product.getDefaultCurrency().toString());
                 statement.setInt(3, 1);
                 statement.setInt(4, shoppingCartId);
                 statement.setInt(5, product.getId());
+                statement.setString(6, product.getName());
+                statement.setString(7, product.getDescription());
+                statement.setString(8, product.getImageWithoutPath());
+                statement.setInt(9, product.getCategoryId());
+                statement.setInt(10, product.getSupplierId());
                 statement.executeUpdate();
+//                shoppingCart.addToShoppingCart(product);
             } else {
                 String sql = "UPDATE cart_item SET quantity = quantity + 1 WHERE product_id = ?";
                 PreparedStatement statement = conn.prepareStatement(sql);
@@ -78,21 +84,80 @@ public class ShoppingCartDaoJdbc implements ShoppingCartDao {
 
     @Override
     public void remove(Product product) {
-        shoppingCart.removeOneItemFromShoppingCart(product);
+        int quantity = getProductQty(product);
+        if(quantity > 1){
+            try (Connection conn = dataSource.getConnection()) {
+                String sql = "UPDATE cart_item SET quantity = quantity - 1 WHERE product_id = ?";
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setInt(1, product.getId());
+                statement.executeUpdate();
+            } catch(SQLException e){
+                throw new RuntimeException(e);
+            }
+        } else if(quantity == 1){
+            deleteItem(product);
+        }
+
     }
 
     @Override
     public HashMap<Product, Integer> getAll() {
-        return shoppingCart.getCart();
+        try (Connection conn = dataSource.getConnection()) {
+            HashMap<Product, Integer> allProducts = new HashMap<>();
+            String sql = "SELECT * FROM cart_item WHERE shopping_cart_id=?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, shoppingCartId);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Product product = new Product(rs.getInt("PRODUCT_ID"),
+                    rs.getString("NAME"),
+                    rs.getString("DESCRIPTION"),
+                    rs.getFloat("PRICE"),
+                    rs.getString("CURRENCY"),
+                    rs.getString("PICTURE"),
+                    rs.getInt("CATEGORY_ID"),
+                    rs.getInt("SUPPLIER_ID")
+                );
+                allProducts.put(product, rs.getInt("QUANTITY"));
+            }
+            return allProducts;
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public ShoppingCart getCart() {
-       return shoppingCart.getShoppingCart();
+        try (Connection conn = dataSource.getConnection()) {
+            shoppingCart.setShoppingCart(getAll());
+            return shoppingCart;
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void deleteCart() {
-        shoppingCart.clearCart();
+        try (Connection conn = dataSource.getConnection()) {
+            HashMap<Product, Integer> allProducts = new HashMap<>();
+            String sql = "DELETE FROM cart_item WHERE shopping_cart_id=?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, shoppingCartId);
+            statement.executeUpdate();
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deleteItem(Product product) {
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "DELETE FROM cart_item WHERE product_id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, product.getId());
+            statement.executeUpdate();
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 }
