9cbe726c19a240333fa8c46e3c8d9118b67ab920
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-08-11 14:03:21 +0200

    [change] toString

--- src/main/java/com/codecool/shop/model/product/Product.java
+++ src/main/java/com/codecool/shop/model/product/Product.java
@@ -105,18 +105,13 @@ public class Product extends BaseModel {
 
     @Override
     public String toString() {
-        return "product";
-//        String.format("id: %1$d, " +
-//                "name: %2$s, " +
-//                "defaultPrice: %3$f, " +
-//                "defaultCurrency: %4$s, " +
-//                "productCategory: %5$s, " +
-//                "supplier: %6$s",
-//            this.id,
-//            this.name,
-//            this.defaultPrice,
-//            this.defaultCurrency.toString(),
-//            this.productCategory.getName(),
-//            this.supplier.getName());
+        return String.format("id: %1$d, " +
+                "name: %2$s, " +
+                "defaultPrice: %3$f, " +
+                "defaultCurrency: %4$s, ",
+            this.id,
+            this.name,
+            this.defaultPrice,
+            this.defaultCurrency.toString());
     }
 }
